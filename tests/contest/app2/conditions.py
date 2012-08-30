a5adf75bfaa8a05657e2873ffb04f4bb4242937f
Dominik Kozaczko <dominik@logrus>
2012-08-30 16:51:43 +0200

    remove the de-facto nonexistent tests

--- tests/contest/app2/conditions.py
+++
@@ -1,8 +0,0 @@
-#coding: utf-8
-from clausula.base import clauses
-
-def resolve(obj):
-    "always Fail"
-    return False
-
-clauses.register(resolve, "always False")
--- tests/contest/app2/models.py
+++
@@ -1,3 +0,0 @@
-#coding utf-8
-from django.db import models
-#from conditions.models import Condition

69280ce0aa9efb130eb73ca257dbeaa25f6d6577
Dominik Kozaczko <dominik@logrus>
2012-08-30 16:57:01 +0200

    add missing LICENSE and simple setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-clausula',
+    version='1.0.0',
+    packages=['clausula', 'clausula.templatetags'],
+    url='https://github.com/dekoza/django-clausula',
+    license='BSD',
+    author='Dominik Kozaczko',
+    author_email='dominik@kozaczko.info',
+    description='Dynamic conditions in relations between objects'
+)
